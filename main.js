cf1ed3ad53cbf81fb8128538502586c327c3c373
Kate0809 <81236887+Kate0809@users.noreply.github.com>
2021-04-22 12:26:52 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,98 @@
 let isMobile = {
-	Android: function() {return navigator.userAgent.match(/Android/i);},
-	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
-	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
-	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
-	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
-	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+  Android: function() {return navigator.userAgent.match(/Android/i);},
+  BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+  iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+  Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+  Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+  any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
 };
-		let body=document.querySelector('body');
+    let body=document.querySelector('body');
 if(isMobile.any()){
-		body.classList.add('touch');
-		let arrow=document.querySelectorAll('.arrow');
-	for(i=0; i<arrow.length; i++){
-			let thisLink=arrow[i].previousElementSibling;
-			let subMenu=arrow[i].nextElementSibling;
-			let thisArrow=arrow[i];
+    body.classList.add('touch');
+    let arrow=document.querySelectorAll('.arrow');
+  for(i=0; i<arrow.length; i++){
+      let thisLink=arrow[i].previousElementSibling;
+      let subMenu=arrow[i].nextElementSibling;
+      let thisArrow=arrow[i];
 
-			thisLink.classList.add('parent');
-		arrow[i].addEventListener('click', function(){
-			subMenu.classList.toggle('open');
-			thisArrow.classList.toggle('active');
-		});
-	}
+      thisLink.classList.add('parent');
+    arrow[i].addEventListener('click', function(){
+      subMenu.classList.toggle('open');
+      thisArrow.classList.toggle('active');
+    });
+  }
 }else{
-	body.classList.add('mouse');
+  body.classList.add('mouse');
 }
+
+
+// Меню бургер
+$(document).ready(function() {
+	$('.nav__icon').click(function(event) {
+		$('.nav__icon,.nav__body').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+$('.nav__body').click(function(){
+    $('.nav__icon, .nav__body').removeClass('active');
+    $('body').removeClass('lock');
+});
+
+
+
+
+
+//button to top
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) { // Show backToTopButton
+    if(!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  }
+  else { // Hide backToTopButton
+    if(backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function() {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+// function backToTop() {
+//   window.scrollTo(0, 0);
+// }
+
+function smoothScrollBackToTop() {
+  const targetPosition = 0;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 750;
+  let start = null;
+  
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function easeInOutCubic(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t + b;
+	t -= 2;
+	return c/2*(t*t*t + 2) + b;
+};
